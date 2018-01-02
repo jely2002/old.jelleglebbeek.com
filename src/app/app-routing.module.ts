7fd326a252087626bc9eaf112b83e3f9f160f948
jely2002 <jw.glebbeek@gmail.com>
2018-01-02 17:20:07 +0100

    Removed project pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { WalksimulatorComponent } from './walksimulator/walksimulator.component';
-import { WebsiteComponent } from './website/website.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'walksimulator', component: WalksimulatorComponent},
-  { path: 'website', component: WebsiteComponent},
   { path: '**', component: MainComponent, redirectTo: '' }
 ];
 
