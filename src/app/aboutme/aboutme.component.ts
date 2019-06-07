369a948e3fa97a1509a3d2ff60704db366848260
Jelle Glebbeek <jw.glebbeek@gmail.com>
2019-06-07 08:55:23 +0200

    Added code to auto-update age

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -10,6 +10,19 @@ export class AboutmeComponent implements OnInit {
 
   constructor(public analytics: AnalyticsService) { }
 
+  public ageFromDateOfBirthday(dateOfBirth: any): number {
+    const today = new Date();
+    const birthDate = new Date(dateOfBirt);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age --;
+    }
+    return age;
+  }
+  
+  age = ageFromDateOfBirthday("06/09/2002");
+     
   ngOnInit() {
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
