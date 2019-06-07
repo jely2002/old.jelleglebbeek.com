0976e743f177cb8d469aee866c09829b068086bc
Jelle Glebbeek <jw.glebbeek@gmail.com>
2019-06-07 09:28:44 +0200

    Fixed variable typo

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -12,7 +12,7 @@ export class AboutmeComponent implements OnInit {
 
   public ageFromDateOfBirthday(dateOfBirth: any): number {
     const today = new Date();
-    const birthDate = new Date(dateOfBirt);
+    const birthDate = new Date(dateOfBirth);
     let age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
